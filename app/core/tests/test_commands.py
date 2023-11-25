ac4cfa18f7e3407ac8de33f9674bccbe03c93070
rholdxz <rholdxz@gmail.com>
2023-11-25 23:56:28 +0800

    Fixing Whitespace in wait_for_db.py, test_commands.py and also pass all unused py file.
    Signed-off-by: rholdxz <rholdxz@gmail.com>

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -28,7 +28,9 @@ class CommandTests(SimpleTestCase):
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
         
+
         call_command('wait_for_db')
+
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
