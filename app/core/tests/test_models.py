463366569d54e5498831c7f9c537d5776368afc2
rholdxz <rholdxz@gmail.com>
2023-11-26 21:00:25 +0800

    Normalize Email Address.
    Signed-off-by: rholdxz <rholdxz@gmail.com>

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,7 +10,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a new user with an email is successful."""
-        email = 'test@gmail.com'
+        email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -19,3 +19,15 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized."""
+        sample_emails = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@example.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
