d39819ec74041ab0fef32206717ba30b2c37202c
rholdxz <rholdxz@gmail.com>
2023-11-28 01:37:19 +0800

    Checking Code for DRF API.
    Signed-off-by: rholdxz <rholdxz@gmail.com>

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'rest_framework',
     'drf_spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
