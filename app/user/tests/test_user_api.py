477f78a2adb91304265a6b671113ab5c550356f8
rholdxz <rholdxz@gmail.com>
2023-12-07 10:39:24 +0800

    Failed.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -52,13 +52,13 @@ class PublicUserApiTests(TestCase):
     def test_password_too_short_error(self):
         """Test as error is returned if password less than 5 chars."""
         payload = {
-            'email': 'test@example',
+            'email': 'test@example.com',
             'password': 'pw',
             'name': 'Test name',
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status.code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
