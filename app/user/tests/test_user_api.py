5ca45bab5e3086448fd006d0a2fb1a8de0f9afe8
rholdxz <rholdxz@gmail.com>
2023-12-07 14:55:29 +0800

    Update December.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -40,7 +40,7 @@ class PublicUserApiTests(TestCase):
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
         payload = {
-            'email': 'test@example.com',
+            'email': 'net@example.com',
             'password': 'testpass123',
             'name': 'Test Name',
         }
--- app/user/urls.py
+++ app/user/urls.py
@@ -7,6 +7,7 @@ from user import views
 
 
 app_name = 'user'
+
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
 ]
