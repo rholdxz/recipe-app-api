a9423317985b17fcf6c5f5b90b0a5bd8e255d0a6
rholdxz <rholdxz@gmail.com>
2023-12-19 00:30:27 +0800

    Deploying AWS Virtual Server

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,7 +7,7 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module."""
+    """Tests the calc module."""
 
     def test_add_numbers(self):
         """Test adding numbers together."""
